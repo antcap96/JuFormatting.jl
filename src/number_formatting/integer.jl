a24549f87d82dad07d0d036b0c057df681db4a82
António Capela <acapela96@hotmail.com>
2021-02-14 17:37:16 +0000

    use a UInt8 vector prealocated

--- src/number_formatting/integer.jl
+++ src/number_formatting/integer.jl
@@ -17,35 +17,43 @@ _div(x::Integer, ::_Bin) = x >> 1
 _div(x::Integer, ::_Oct) = x >> 3
 _div(x::Integer, ::Union{_Hex, _HEX}) = x >> 4
 
-_prefix(::_Dec) = ""
-_prefix(::_Hex) = "0x"
-_prefix(::_HEX) = "0X"
-_prefix(::_Oct) = "0o"
-_prefix(::_Bin) = "0b"
+_prefix(::_Dec) = Vector{UInt8}("")
+_prefix(::_Hex) = Vector{UInt8}("0x")
+_prefix(::_HEX) = Vector{UInt8}("0X")
+_prefix(::_Oct) = Vector{UInt8}("0o")
+_prefix(::_Bin) = Vector{UInt8}("0b")
 
-_digitchar(x::Integer, ::_Bin) = Char(x == 0 ? '0' : '1')
-_digitchar(x::Integer, ::_Dec) = Char('0' + x)
-_digitchar(x::Integer, ::_Oct) = Char('0' + x)
-_digitchar(x::Integer, ::_Hex) = Char(x < 10 ? '0' + x : 'a' + (x - 10))
-_digitchar(x::Integer, ::_HEX) = Char(x < 10 ? '0' + x : 'A' + (x - 10))
+_digitchar(x::Integer, ::_Bin) = UInt8(x == 0 ? '0' : '1')
+_digitchar(x::Integer, ::_Dec) = UInt8('0' + x)
+_digitchar(x::Integer, ::_Oct) = UInt8('0' + x)
+_digitchar(x::Integer, ::_Hex) = UInt8(x < 10 ? '0' + x : 'a' + (x - 10))
+_digitchar(x::Integer, ::_HEX) = UInt8(x < 10 ? '0' + x : 'A' + (x - 10))
 
 
 function format_integer(val, base, has_prefix::Bool)
-    result = has_prefix ? _prefix(base) : ""
+    prefix = has_prefix ? _prefix(base) : UInt8[]
 
     # find largest power that fits in `val`, subtract it from `val`
     # repeat the process until `val` is 0
 
     lowerbound = _div(val, base)
     power = one(val)
+    digits = 1
     while power <= lowerbound
         power = _mul(power, base)
+        digits += 1
     end
+
+    prefix_len = length(prefix)
+    arr = Vector{UInt8}(undef, digits + prefix_len)
+    arr[1:prefix_len] = prefix
+    i = 1 + prefix_len
     remainder = val
     while power > 0
         (quotient, remainder) = divrem(remainder, power)
-        result *= _digitchar(quotient, base)
+        arr[i] = _digitchar(quotient, base)
+        i += 1
         power = _div(power, base)
     end
-    return result
+    return String(arr)
 end
